4db5f4abf45a3dc4ac534719eb35abdc37837137
csrvermaak <csrvermaak@hotmail.com>
2015-10-08 11:10:10 +0200

    Update with Deploy Keys

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ cd out
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "<you>@<your-email>"
+git config user.name "csrvermaak"
+git config user.email "<csrvermaak@hotmail.com>"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
